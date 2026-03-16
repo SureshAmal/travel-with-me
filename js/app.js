18301bc6733a9e8f0ff9dc852db10dc7f180ef2f
sureshamal <sureshambechada@proton.me>
2026-03-16 23:00:42 +0530

    feat: Enable Angular HTML5 mode routing by updating `$locationProvider` and all internal links, and remove an unused CSS class.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 var app = angular.module('travelBlogApp', ['ngRoute', 'ngAnimate', 'ngSanitize']);
 
-app.config(['$routeProvider', function($routeProvider) {
+app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+    $locationProvider.html5Mode(true);
     $routeProvider
         .when('/', {
             templateUrl: 'views/home.html',
